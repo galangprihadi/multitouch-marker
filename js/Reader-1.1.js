f6e4ae5a363dfbf97eb49c31b59b39aa9c236c6b
galangprihadi <galang.prihadi@gmail.com>
2025-09-30 18:32:16 +0900

    Enhance scanner functionality and styling
    - Updated CSS for scanner elements to improve layout and appearance.
    - Modified HTML to remove placeholder text from scanner divs.
    - Enhanced JavaScript to dynamically set text content for scanner markers and manage touch events more effectively.

--- js/Reader-1.1.js
+++ js/Reader-1.1.js
@@ -10,19 +10,37 @@ const reader = [];
 class Reader {
     constructor (param) {
         this.scanner = document.getElementById(param.id);
+        this.scanText = document.createElement("span");
+        this.scanner.append(this.scanText);
 
         if (param.posX) this.scanner.style.left = param.posX;
         if (param.posY) this.scanner.style.top = param.posY;
         if (param.color) this.scanner.style.backgroundColor = param.color;
+        if (param.text) this.scanText.textContent = param.text;
 
-        this.isActive = true;
-        this.lastTouches = 0;
+        this.showMark = true;
+
+        this.distance = 0;
+        this.numOfTouch = 0;
+
+        this.markers = {};
+        this.lastTouches = [];
         this.touchPos = [];
 
         this.scanner.addEventListener("touchstart", (event) => {
             this.readTag(event);
-        })
+        });
+
+        this.scanner.addEventListener("touchmove", (event) => {
+            this.readTag(event);
+        });
+
+        this.scanner.addEventListener("touchend", (event) => {
+            this.readTag(event);
+        });
     }
+
+    // ======================================================================================= READ TAG FUNCTION
 
     readTag (event) {
         event.preventDefault();
@@ -30,12 +48,65 @@ class Reader {
         this.lastTouches = Array.from(event.touches).filter(touch => touch.target === this.scanner);
         this.touchPos = [];
 
-        if (this.isActive) {
-            for (let i=0; i < this.lastTouches.length; i++) {
+        this.numOfTouch = this.lastTouches.length;
+
+
+        // ========================================================================== READ MULTITOUCH MARKER TIPS
+
+        if (this.numOfTouch > 0) {
+            for (let i=0; i < this.numOfTouch; i++) {
+                const touch = this.lastTouches[i];
+                const touchId = touch.identifier;
+
+                // Read touch position
+                const x = touch.clientX - this.scanner.getBoundingClientRect().left;
+                const y = touch.clientY - this.scanner.getBoundingClientRect().top;
+
+                this.touchPos.push({x, y});
                 
+                // Draw markers (red dot)
+                if (this.showMark) {
+                    if (!this.markers[touchId]) {
+                        const point = document.createElement("div");
+                        point.classList.add("tip-marker");
+                        this.scanner.appendChild(point);
+                        this.markers[touchId] = point;
+                    }
+
+                    this.markers[touchId].style.left = `${x}px`;
+                    this.markers[touchId].style.top = `${y}px`;
+                }
+            }
+        }
+        else {
+            // Remove markers (red dot)
+            Object.keys(this.markers).forEach((keyId) => {
+                if (this.markers[keyId]) {
+                    this.scanner.removeChild(this.markers[keyId]);
+                    delete this.markers[keyId];
+                }
+            });
+        }
+
+
+        // ========================================================================== MEASURE THE DISTANCE
+
+        this.distance = 0;
+
+        if (this.numOfTouch >= 2) {
+            for (let i=0; i < this.numOfTouch; i++) {
+                for (let j= i+1; j < this.numOfTouch; j++) {
+                    const dx = this.touchPos[j].x - this.touchPos[i].x;
+                    const dy = this.touchPos[j].y - this.touchPos[i].y;
+                    const tempDistance = Math.round(Math.sqrt((dx * dx) + (dy * dy)));
+
+                    if (tempDistance > this.distance) {
+                        this.distance = tempDistance;
+                    }
+                }
             }
         }
 
-        console.log(this.lastTouches);
+        this.scanText.textContent = `${this.distance} px  |  (${this.numOfTouch} touches)`;
     }
 }
