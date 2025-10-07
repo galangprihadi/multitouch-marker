52e19cead0bb3825b0e77d89482a4b0719b94eea
galangprihadi <galang.prihadi@gmail.com>
2025-10-07 15:15:09 +0900

    Update distance settings and text result for Device 2 in phase-1; adjust min and max distance values for improved accuracy

--- experiment/phase-1.js
+++ experiment/phase-1.js
@@ -88,11 +88,11 @@ btnDev2.addEventListener("click", () => {
     btnDev3.className = "passive";
 
     capturedData = "";
-    textResult.textContent = "Experiment Phase 1 (Device 2) (128/285)";
+    textResult.textContent = "Experiment Phase 1 (Device 2) (125/288)";
 
     scanner.setId({
-        minDistance: 128,
-        maxDistance: 285,
+        minDistance: 125,
+        maxDistance: 288,
     });
 });
 
