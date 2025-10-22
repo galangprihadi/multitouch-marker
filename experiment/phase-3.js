59920682a1525973fe3b32a2c54db49abf1a1630
galangprihadi <galang.prihadi@gmail.com>
2025-10-22 10:32:12 +0900

    .

---
+++ experiment/phase-3.js
@@ -0,0 +1,146 @@
+/////////////////////////////////////////////////////////////////////
+//                         Initialization                          //
+/////////////////////////////////////////////////////////////////////
+
+// Scanner Initialization
+const scanner = new Scanner({
+    element: "scanner",
+    text: "Testing...",
+    posX: "28vw",       // 5vw - 50vw
+    posY: "35vh",       // 25vh - 60vh
+    width: "44vw",
+    height: "44vw",
+    rotate: "0deg",     // -20deg - 20deg    
+    bgColor: "#f5720055",
+    // bgImage: "url(../images/bgScanner.png)",
+    bgActive: "url(../images/bgActive.png)",
+    devMode: true,
+});
+
+// Scanner ID Setup
+scanner.setId({
+    minDistance: 70,
+    maxDistance: 140,
+});
+
+const textResult = document.getElementById("text-result");
+textResult.textContent = "Experiment Phase 3 (Device 1) (70/140)";
+
+let scanResult = {};
+let capturedData = "";
+let numOfData = 0;
+
+// Form Data
+const formAction = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfMFIcZq-_ZKP-GQp1WOYFahD3VwQxTdhqWo43Gqa_utXsvAA/formResponse";
+const entry = "entry.303837952";
+
+
+/////////////////////////////////////////////////////////////////////
+//                        Button Functions                         //
+/////////////////////////////////////////////////////////////////////
+
+function btnReset() {
+    window.location.reload();
+}
+
+function btnClose() {
+    window.location.href = "../index.html";
+}
+
+function btnSave() {
+    if (formAction && entry && capturedData != "") {
+        const formData = new URLSearchParams();
+        formData.append(entry, capturedData);
+
+        fetch(formAction, {
+            method: 'POST',
+            mode: 'no-cors',
+            body: formData,
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+
+        textResult.textContent = `Data saved!\nNum of Data: ${numOfData}\n${capturedData}`;
+    }
+}
+
+const btnDev1 = document.getElementById("btnDev1");
+const btnDev2 = document.getElementById("btnDev2");
+const btnDev3 = document.getElementById("btnDev3");
+
+btnDev1.addEventListener("click", () => {
+    btnDev1.className = "";
+    btnDev2.className = "passive";
+    btnDev3.className = "passive";
+
+    capturedData = "";
+    numOfData = 0;
+    textResult.textContent = "Experiment Phase 3 (Device 1) (70/140)";
+
+    scanner.setId({
+        minDistance: 70,
+        maxDistance: 140,
+    });
+});
+
+btnDev2.addEventListener("click", () => {
+    btnDev1.className = "passive";
+    btnDev2.className = "";
+    btnDev3.className = "passive";
+
+    capturedData = "";
+    numOfData = 0;
+    textResult.textContent = "Experiment Phase 3 (Device 2) (117/223)";
+
+    scanner.setId({
+        minDistance: 117,
+        maxDistance: 223,
+    });
+});
+
+btnDev3.addEventListener("click", () => {
+    btnDev1.className = "passive";
+    btnDev2.className = "passive";
+    btnDev3.className = "";
+
+    capturedData = "";
+    numOfData = 0;
+    textResult.textContent = "Experiment Phase 3 (Device 3) (80/160)";
+
+    scanner.setId({
+        minDistance: 80,
+        maxDistance: 160,
+    });
+});
+
+
+
+/////////////////////////////////////////////////////////////////////
+//                        Read Every Frame                         //
+/////////////////////////////////////////////////////////////////////
+
+function frameLoop() {
+    if (scanner.getData(scanResult)) {
+        capturedData += `${scanResult.id},${scanResult.minDistance},${scanResult.maxDistance} _ `;
+        numOfData += 1;
+        textResult.textContent = `Num of Data: ${numOfData}\n${capturedData}`;
+
+        const randX = Math.floor(Math.random() * (50 - 5 + 1) + 5);     // 5vw - 50vw
+        const randY = Math.floor(Math.random() * (60 - 25 + 1) + 25);   // 25vh - 60vh
+        const randR = Math.floor(Math.random() * (20 + 20 + 1) - 20);   // -20deg - 20deg
+
+        // Reposition
+        scanner.setMarker({
+            posX: `${randX}vw`,       
+            posY: `${randY}vh`,       
+            rotate: `${randR}deg`,       
+        });
+    }
+
+    requestAnimationFrame(() => {
+        frameLoop();
+    });
+}
+
+frameLoop();
